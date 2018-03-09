deed28531ed2c3a60e2e4b1252ba5f24e6a18403
785348023@qq.com <785348023@qq.com>
2018-03-09 10:22:55 +0800

    增加生产环境，生产dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,79 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-module.exports = {
-    entry: {
-        app: './app/main.js'
-    },
-    devServer: {
-        clientLogLevel: 'warning',
-        hot: true,
-        compress: true,
-        port: 9000,
-        publicPath: '/',
-        quiet: false
-    },
-    output: {
-        filename: '[name].min.js',
-        path: path.resolve(__dirname, './dist'),
-        publicPath: '/'
-    },
-    module: {
-        rules: [{
-                test: /\.vue$/,
-                loader: 'vue-loader',
-                options: {
-                    loaders: {
-                        scss: ExtractTextPlugin.extract({
-                            use: 'css-loader!px2rem-loader?remUnit=40&remPrecision=8!sass-loader',
-                            fallback: 'vue-style-loader'
-                        })
-                    },
-                    extractCSS: true
-                }
-            },
-            {
-                test: /\.scss/,
-                loader: 'style-loader!css-loader!px2rem-loader?remUnit=40&remPrecision=8!sass-loader'
-            }
-        ]
-    },
-    plugins: [
+module.exports = env => {
+    console.log(env);
+    if (!env) {
+        env = {};
+    }
+    let plugins = [
+        new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({
-            title: 'hah',
             filename: 'index.html',
             template: './index.html'
         }),
         new webpack.HotModuleReplacementPlugin(),
         new webpack.NamedModulesPlugin(),
         new ExtractTextPlugin("style.css")
-    ],
-    resolve: {
-        extensions: ['.js', '.vue'],
-        alias: {
-            'vue$': 'vue/dist/vue.esm.js'
+    ];
+    if (env.production) {
+        plugins.push(
+            new webpack.DefinePlugin({
+                'process.env': {
+                    NODE_ENV: '"production"'
+                }
+            })
+        );
+    }
+    return {
+        entry: {
+            app: './app/main.js'
+        },
+        devServer: {
+            clientLogLevel: 'warning',
+            hot: true,
+            compress: true,
+            port: 9000,
+            publicPath: '/',
+            quiet: false
+        },
+        output: {
+            filename: '[name].min.js',
+            path: path.resolve(__dirname, './dist'),
+            publicPath: '/'
+        },
+        module: {
+            rules: [{
+                    test: /\.vue$/,
+                    loader: 'vue-loader',
+                    options: {
+                        cssModules: {
+                            localIdentName: '[path][name]---[local]---[hash:base64:5]',
+                            camelCase: true
+                        },
+                        loaders: {
+                            scss: ExtractTextPlugin.extract({
+                                use: 'css-loader!px2rem-loader?remUnit=40&remPrecision=8!sass-loader',
+                                fallback: 'vue-style-loader'
+                            })
+                        },
+                        extractCSS: true
+                    }
+                },
+                {
+                    test: /\.scss/,
+                    loader: 'style-loader!css-loader!px2rem-loader?remUnit=40&remPrecision=8!sass-loader'
+                }
+            ]
+        },
+        plugins: plugins,
+        resolve: {
+            extensions: ['.js', '.vue'],
+            alias: {
+                'vue$': 'vue/dist/vue.esm.js'
+            }
         }
     }
 }
