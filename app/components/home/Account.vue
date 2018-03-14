2efda3e6c3cb83cc0d228362c8ffc19ce58696f9
785348023@qq.com <785348023@qq.com>
2018-03-14 14:06:13 +0800

    暂时  账户

--- app/components/home/Account.vue
+++ app/components/home/Account.vue
@@ -1,15 +1,48 @@
 <template>
-    <div>
-        我是账户
+    <div class="account">
+        <div class="a-header">
+            <img src="" alt="">
+            <p>郭晓波</p>
+        </div>
+        <div class="a-list">
+            <a href="#" >
+                <span class="left">余额</span>
+                <span class="right">0.00</span>
+            </a>
+            <a href="#" >
+                <span class="left">账单</span>
+            </a>
+            <a href="#" >
+                <span class="left">银行卡</span>
+            </a>
+            <a href="#" >
+                <span class="left">代金券</span>
+            </a>
+            <a href="#" >
+                <span class="left">设置</span>
+            </a>
+            <a href="#" >
+                <span class="left">推荐好友</span>
+            </a>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
-        
+        data() {
+            return {
+                key: value
+            }
+        },
     }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+.account{
+    padding:0;
+    .a-header{
+        height: 200px;
+    }
+}
 </style>
